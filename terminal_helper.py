c390d51e143eba519e38cab1a9b55867e8b2b3ba
Jonny <jonny@macmini>
2020-12-13 12:48:16 +0100

    a bit of changes

--- terminal_helper.py
+++ terminal_helper.py
@@ -5,11 +5,18 @@ import subprocess as sp
 from subprocess import call as spc
 from subprocess import check_output as check_output
 import sys
-from time import sleep
+from time import sleep as sleep
 import glob
 import shutil
 from shutil import rmtree as rmtree
-
+from getpass import getpass
+from shutil import disk_usage as du
+from shutil import unpack_archive as unpack
+from shutil import _make_zipfile as zip
+from shutil import copyfile as copyfile
+from os import getcwd as getcwd
+from os.path import isfile
+from os.path import isdir
 class Terminal_helper():
     
 
@@ -24,14 +31,66 @@ class Terminal_helper():
         self.cd=subprocess.call(["cd", ".."], shell=True)
         self.cls=subprocess.call(["clear"])
         self.print_on_the_menu="test"
+        self.print_on_the_menu1="test"
         self.print_on_the_menu2="test"
         self.print_on_the_menu3="test"
+        self.menu_needed=False
     
+    #path not working if you change in bash
+    def bash(self):
+        spc(["bash"], shell=True)
+        print(getcwd())
+        self.pwd=spc(["pwd"], shell=True)
+        pwd_str=str(check_output(["pwd"], shell=True, )).strip('\'' '\\b' '\\n' '\'' )
+        self.pwd_str=pwd_str
+        self.counter=99
+        self.path_dict.update({self.counter:(self.pwd_str)})
+        print(self.pwd_str)
+    
+    def write_note(self):
+        note_name=input("input filename")
+        #the current working directory will be pwd
+        pwd=getcwd()
+        #new_name will be cwd+/+inputed note_name
+        new_name=pwd+"/"+note_name
+        # if path provided already is a file:
+        if isfile(new_name):
+            try: 
+                #open the file in read mode
+                file=open(new_name, 'r')
+                #take input from keyboard and present the file content in input prompt
+                note_input=input(file.read())
+                #close the file 
+                file.close()
+                #open the file in append mode
+                file=open(new_name, 'a')
+                #write the input to the file
+                file.write(note_input)
+                #close the file again.
+                file.close()
+                 
+            except:
+                print("no go append")
+        
+        if isfile(new_name)==False:
+            try:
+                file=open(new_name, 'w+')
+                note_input=input("write something")
+                file.write(note_input)
+                file.close()
+            except:
+                print("shit pomesfrites")
+
+
+
+    def sudo(self):
+        pass 
+
     def th_help(self):
-        print("as the names states this is a terminal helper for linux you could print stuff and delete stuff.")
+        print("as the names states this is a terminal helper for linux you could print stuff and delete stuff. for writing a note print notepad for cd.. print left arrow for cd / print left arrow twice for going to privios dir press right key for home press right key twice for open a file press o. Delete press o and if you get bored print bash")
 
     def clear_screen(self):
-        spc('clear', shell=True)
+        spc(["bash"], shell=True)
         
     #FILE SECTION
     #dir files and view them
@@ -65,7 +124,12 @@ class Terminal_helper():
     #should show current location
     def current_location(self):
         print(self.pwd_str)
+        print("-->")
+        if os.name=='nt':
+            print(os.getcwd)
+            os.get
         return self.pwd_str
+
         
         #should bring you to previous dir
     def move_forward(self):
@@ -120,6 +184,7 @@ class Terminal_helper():
         self.current_dir=os.listdir()
         print(self.current_dir)
         return self.current_dir
+        self.print_on_the_menu1=du(self.pwd_str)
 
     def remove_file(self):
        
@@ -160,6 +225,8 @@ class Terminal_helper():
             self.file_name=open((file_path+"/"+file_name), 'w+')
         except:
             print("no go")
+    def create_dir(self, path):
+        pass
 
     def whoami(self):
         subprocess.call(["whoami"], shell=True)
@@ -170,7 +237,7 @@ class Terminal_helper():
         self.print_on_the_menu=print_on_the_menu 
         
         print("-------------------------------------------------------------------------------------------------------------------------------------")
-        print("Terminal Helper  " +self.pwd_str + "     "+ (str(self.show_date()))+"  ")
+        print("Terminal Helper  " +self.pwd_str + "     "+ (str(self.show_date()))+ (self.print_on_the_menu1) +"  ")
         print(" options are < =cd..  >=fw  l=list o=open  c=create file d=delete  q for quit   "+ (print_on_the_menu)) 
         print("-------------------------------------------------------------------------------------------------------------------------------------")
     
@@ -178,12 +245,19 @@ class Terminal_helper():
     
     
     def keyboard_input(self):
+        menu_counter=0
         self.running=""
         spc('clear')
         if int(self.meaning_of_life)!=42:
             quit()
         while self.running !="q":
-            self.main_menu(self.print_on_the_menu)
+            self.main_menu("Hej")
+            menu_counter+=1
+            self.write_on_the_menu="welcome"
+            if menu_counter==1:
+                self.main_menu(self.print_on_the_menu)
+            if self.menu_needed==True:
+                self.main_menu(self.print_on_the_menu)
             self.running=input()
             if self.running=="l":
                 spc('clear')
@@ -207,6 +281,8 @@ class Terminal_helper():
                 self.list_files()
                 continue
             if self.running=="^[[H":
+                spc('clear')
+                self.main_menu("home")
                 self.home()
                 continue
 
@@ -223,6 +299,7 @@ class Terminal_helper():
                 spc('clear', shell=True)
                 self.main_menu(self.print_on_the_menu)
                 self.file_reader()
+                self.menu_needed=False
                 continue
             if self.running=="d":
                 spc('clear', shell=True)
@@ -230,7 +307,9 @@ class Terminal_helper():
                 self.remove_file() 
             if self.running=="c":
                 spc('clear', shell=True)
-                self.create_file(self.pwd_str, (input("filename")))
+                self.print_on_the_menu="WRITE A FILE NAME"
+                self.main_menu(self.print_on_the_menu)
+                self.create_file(self.pwd_str, (input("")))
                 continue
             if self.running=="\x1b[C\x1b[C":
                 self.print_on_the_menu=("not so fast ok ok bringing you home")
@@ -239,6 +318,11 @@ class Terminal_helper():
             if self.running=="h" or self.running=="-h" or self.running=="help":
                 self.th_help()
                 continue
+            if self.running=="bash":
+                self.bash()
+            if self.running=="notepad":
+                self.write_note()
+                
             else:
                 self.print_on_the_menu=("This is a bit complicated.. try again. For cd .. press left arrow key")
                 spc('clear')
