52e36c4f4268d91468ed23807ac8d7b70a963625
Jonny <jonny@macmini>
2020-12-14 21:39:10 +0100

    asdf

--- linux_terminal_helper.py
+++ linux_terminal_helper.py
@@ -35,6 +35,8 @@ class Terminal_helper():
         self.menu_needed = False
         self.linux_init = ""
 
+        
+
     # just starting bash from the program
     def bash(self):
         spc(["bash"], shell=True)
@@ -85,7 +87,7 @@ class Terminal_helper():
 
     def th_help(self):
         print(
-            "as the names states this is a terminal helper for linux you could print stuff and delete stuff. for writing a note print notepad for cd.. print left arrow for cd / print left arrow twice for going to privios dir press right key for home press right key twice for open a file press o. Delete press o and if you get bored print bash")
+            "as the names states this is a terminal helper for linux you could print stuff and delete stuff. for writing notepad cd.. print left arrow for cd / print left arrow twice for going to privios dir press right key for home press right key twice for open a file press o. Delete press o and if you get bored print bash")
 
     def clear_screen(self):
         spc(["bash"], shell=True)
@@ -320,9 +322,8 @@ class Terminal_helper():
         if int(self.meaning_of_life) != 42:
             quit()
         while self.running != "q":
-            self.main_menu("Hej")
             menu_counter += 1
-            self.write_on_the_menu = "welcome"
+            self.write_on_the_menu = "welcome if menu is missing press <>^ once or twice followed by enter"
             if menu_counter == 1:
                 self.main_menu(self.print_on_the_menu)
             if self.menu_needed == True:
@@ -362,45 +363,108 @@ class Terminal_helper():
                 self.list_files()
                 continue
 
-            # if self.running=="x1b[3~":
-            # print("delete key")
+            if self.running=="x1b[3~":
+                print("delete key function not enabled for safetyreasons")
+                self.main_menu("")    
             if self.running == "open" or self.running == "o" or self.running == "\x1b[A" or self.running == "^[[A":
                 spc('clear', shell=True)
                 self.main_menu(self.print_on_the_menu)
                 self.file_reader()
                 self.menu_needed = False
+                sleep(0.2)
+                self.print_on_the_menu="exiting open"
+                self.main_menu(self.print_on_the_menu)
                 continue
             if self.running == "d":
                 spc('clear', shell=True)
                 self.main_menu("")
                 self.remove_file()
+                self.main_menu("exiting delete")
+                continue
             if self.running == "c":
                 spc('clear', shell=True)
                 self.print_on_the_menu = "WRITE A FILE NAME"
                 self.main_menu(self.print_on_the_menu)
                 self.create_file(self.pwd_str, (input("")))
+                spc('clear')
+                self.main_menu("exiting c")
+                continue
                 continue
             if self.running == "\x1b[C\x1b[C":
                 self.print_on_the_menu = ("not so fast ok ok bringing you home")
                 self.home()
+                spc('clear')
+                self.main_menu("exiting cd")
                 continue
             if self.running == "h" or self.running == "-h" or self.running == "help":
                 self.th_help()
                 continue
             if self.running == "bash":
                 self.bash()
-            if self.running == "notepad":
+            if self.running == "notepad" or self.running == "^[[A^[[A" or self.running == "\x1b[A\x1b[A":
                 self.write_note()
+                sleep(0.2)
+                spc('clear')
+                self.main_menu("exit notepad")
+                continue
             if self.running == "ping":
                 self.ping_a_host()
             if self.running == "service":
                 self.list_services()
             else:
-                self.print_on_the_menu = ("This is a bit complicated.. try again. For cd .. press left arrow key")
                 spc('clear')
+                self.main_menu(self.print_on_the_menu)
+                self.print_on_the_menu = ("this is a bit complicated press left,right or upp key one or twice and press enter")
 
 
 spc(["clear"])
 th = Terminal_helper("42")
 # th.change_owner()
 th.keyboard_input()
+
+"""
+Rapport till uppgift 2 i Linux och skriptspråk av Jonny Värn.
+Jag använder mig av VIM med plugin airline, justify, matchit, powerline och python-jedi för att få en trevlig utvecklingsmiljö med autokomplettering och lite finare vim.                
+Jag använder mig av VIM eftersom jag sitter på en Linux konsol via ssh.
+Jag hade använt en annan terminalbaserad kanske emacs eftersom jag valt att inte köra grafiskt. Om jag hade kört grafiskt så hade jag antagligen kört visualstudiocode, pycharm, atom eller någon annan IDE.
+Jag valde filhanterare eftersom den låg på först och verkade vara den mest användbara applikationen. 
+Filhanteraren började växa och utvecklades till Linux-terminal-helper när jag tänkte att den skulle kunna vara lite stöd för andra när de jobbar i Linux/Unix möjligtvis finns det en  en framtid i windows i terminalmiljö.  
+Linux är ju egentligen en Kärna Linux/GNU är ett operativsystem och det finns många olika varianter av GNU/Linux några av de största listas nedan.
+RHEL eller RedhatEnterprise där även Linus Torvalds huserar med community drivna varianter som Centos.
+Debian med varianter som Ubuntu som nästan har gått om Debian i antal användare.
+Suse Enterprise Linux som från början startade i Tyskland ursprungligen från Slackware Linux.
+Det alla Linux varianter delar är som sagt Linux kärnan.
+I alla Linux varianter kan man installera .tgz paket eller så kallade tar bollar.
+Enklast kan vara att dela upp Linux i vilken pakethanterare de använder för att lättare se släktskapet.
+RHEL och Centos är i princip samma Os förutom enterprise supporten och de använder sig av pakhethanteraren yum som kom ifrån yellowdog-linux som var det enda linux som fungerade för mac powerpc på den tiden det begav sig. 
+En lite mer krånglig variant är rpm RedhatPacketManager där man själv får hålla ordning på vilka dependencies som behövs.
+
+Debian, ubuntu, ubuntu mate etc använder sig av pakethanteraren apt. Eller den lite krångligare varianten där man får använda sig av dependencies själv dpkg
+
+Av de som står själva förtjänar Alpine linux att nämnas ett minimalt komplett Linux/GNU som från början var avsett att användas på accesspunkter och mindre inbyggda enheter.
+
+
+Vad finns det för användningsområden inom Linux?
+Användningsområden inom Linux är oändliga men för att ge några exempel kan man tala om olika typer av servrar exempelvis DHCP-server.
+Det finns olika styrdatorer.
+Jag skulle ju klassificera Android som en Linux kärna så telefoner är ju ett annat område där vi använder Linux.
+
+Beskriv hur ni använt er av Linux i denna applikation
+Jag har utvecklat min applikation på antix linux (ett debian derrivat med init istället för systemd)
+Jag har använt Vim och kört den ifrån en Linux terminal, jag har även testat applikationen på ubuntu och en openbsd, vilket inte är linux.
+Beskriv de olika tjänster ni känner till som finns i Linux
+Ja eftersom en tjänst är ett program som körs i bakgrunden så blir ju svaret nästan oändligt.
+Jag väljer att lista några av de vanligaste.
+Brandväggar firewalld, ufw, ip-tables. Dessa är tjänster som kan kontrollera vilka TCP/UDP portar som är öppna in eller ut på Linux.
+FTP File Transfer Protocol som erbjuder fildelning. Det finns såklart en uppsjö utav FTP varianter.
+ Cron/cronnyd etc är en tjänst som startar tjänster efter tid.
+ Cups erbjuder delning av skrivare och skrivartjänster.
+ Ntp är NetworkTimeProtocol och erbjuder tidssynkronisering.
+ Systemlog tjänster som loggar händelser på systemet. några exempel är syslog-ng och rsyslog.
+ SSH secureShell bjuder möjlighet att logga in på en dator via TCP/IP och är ersättaren till det mindre säkrare Telnet. Det ju många olika varianter av ssh servrar men min favorit kommer från en av mina favorit operativsystem openbsd och heter openssh.
+
+  LinuxTerminalHelper är ett embryo för närvarande så är det en filhanterare med funktionerna 
+  Notepad(write_note) som öppnar en fil och låter användaren skriva till den och sedan stänga.
+  Flytta runt med hjälp av vänster, höger och uppåtpil. Skapa fil och ta bort fil med c respektive d lista filer med l.resterande funktioner är inte implementerade och bör därför inte användas.
+Skriv en applikation som tillåter dig att flytta runt i filsystemet på din dator. Som en användare skall man kunna gå in i mappar, backa ur mappar, lista filer, skapa filer.
+"""
